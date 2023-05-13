c355a357415f7498ced5f9dddc369432c413caec
gomachan_7 <tt@azrs.jp>
2023-05-14 00:08:40 +0900

    Apply event edit to score builder

--- src/Scene/ScoreEditor/ScoreBuilder.ts
+++ src/Scene/ScoreEditor/ScoreBuilder.ts
@@ -1,8 +1,11 @@
+import {EditorConstants} from '@/Scene/ScoreEditor/EditorConstants';
 import {SongRecource} from '@/Scene/ScoreEditor/SongRecource';
 import {Amson} from '@/Score/ScoreTypes';
+import {ScoreUtility} from '@/Score/ScoreUtility';
 
 export class ScoreBuilder {
   private amson: Amson.Structure;
+  private utility: ScoreUtility;
 
   constructor(resource: SongRecource) {
     this.amson = {
@@ -21,6 +24,8 @@ export class ScoreBuilder {
       appeals: [],
       fevers: [],
     };
+
+    this.utility = new ScoreUtility(this.score);
   }
 
   get score() {
@@ -67,7 +72,195 @@ export class ScoreBuilder {
     }
   }
 
+  addSection(tick: number): boolean {
+    // 上限チェック
+    if (this.amson.sections.length >= Amson.Constants.SectionMax) {
+      return false;
+    }
+    // コンフリクトチェック(追加時のデフォルトの長さを利用)
+    const sectionLengthTick = this.amson.info.resolution * 4 * EditorConstants.DefaultSectionBar;
+    if (!this.checkRangeConflict(tick, tick + sectionLengthTick)) {
+      return false;
+    }
+    // durationチェック
+    if (!this.isValidDuration(tick, tick + sectionLengthTick)) {
+      return false;
+    }
+
+    this.amson.sections.push({
+      type: this.defaultLaneType(),
+      tick: tick,
+      lengthTick: sectionLengthTick,
+    });
+
+    return true;
+  }
+
+  addSectionLength(tick: number): boolean {
+    const found = this.amson.sections.find((section) => section.tick === tick);
+    if (found == null) {
+      return false;
+    }
+
+    // checkRangeConflict()で正しくチェックするために一旦消す
+    this.removeSection(tick);
+
+    const lengthToAdd = this.amson.info.resolution / 4;
+    const from = tick;
+    const to = tick + found.lengthTick + lengthToAdd;
+    const conflict = !this.checkRangeConflict(from, to);
+
+    // コンフリクトしてたら元に戻す、そうでなければTickを伸ばしたSectionを追加
+    this.amson.sections.push({
+      type: found.type,
+      tick: tick,
+      lengthTick: conflict ? found.lengthTick : found.lengthTick + lengthToAdd,
+    });
+
+    return !conflict;
+  }
+
+  subSectionLength(tick: number): boolean {
+    const found = this.amson.sections.find((section) => section.tick === tick);
+    if (found == null) {
+      return false;
+    }
+
+    found.lengthTick = Math.max(
+      this.amson.info.resolution * 4 * 1, // 1小節を下限としておく(変更可能)
+      found.lengthTick - this.amson.info.resolution / 4
+    );
+    return true;
+  }
+
+  toggleSectionType(tick: number): boolean {
+    const found = this.amson.sections.find((section) => section.tick === tick);
+    if (found == null) {
+      return false;
+    }
+
+    switch (this.amson.info.stage) {
+      case Amson.StageType.Live:
+        found.type = (found.type + 1) % (Object.keys(Amson.LiveLaneType).length / 2);
+        break;
+      case Amson.StageType.Fashion:
+        found.type = (found.type + 1) % (Object.keys(Amson.FashionLaneType).length / 2);
+        break;
+      case Amson.StageType.Dance:
+        found.type = (found.type + 1) % (Object.keys(Amson.DanceLaneType).length / 2);
+        break;
+    }
+    console.log(found.type);
+
+    return true;
+  }
+
+  removeSection(tick: number) {
+    this.amson.sections = this.amson.sections.filter((section) => section.tick !== tick);
+    return true;
+  }
+
+  addAppeal(tick: number): boolean {
+    // 上限チェック
+    if (this.amson.appeals.length >= Amson.Constants.AppealMax) {
+      return false;
+    }
+    // コンフリクトチェック
+    const appealLength = this.utility.tickByDuration(Amson.Constants.AppealDuationSec);
+    if (!this.checkRangeConflict(tick, tick + appealLength)) {
+      return false;
+    }
+    // durationチェック
+    if (!this.isValidDuration(tick, tick + appealLength)) {
+      return false;
+    }
+
+    this.amson.appeals.push({tick: tick});
+    return true;
+  }
+
+  removeAppeal(tick: number): boolean {
+    this.amson.appeals = this.amson.appeals.filter((appeal) => appeal.tick !== tick);
+    return true;
+  }
+
+  addFever(tick: number): boolean {
+    // 上限チェック
+    if (this.amson.fevers.length >= Amson.Constants.FeverMax) {
+      return false;
+    }
+    // コンフリクトチェック
+    const feverLength = this.utility.tickByDuration(Amson.Constants.FeverDuraionSec);
+    if (!this.checkRangeConflict(tick, tick + feverLength)) {
+      return false;
+    }
+    // durationチェック
+    if (!this.isValidDuration(tick, tick + feverLength)) {
+      return false;
+    }
+
+    this.amson.fevers.push({tick: tick});
+    return true;
+  }
+
+  removeFever(tick: number): boolean {
+    this.amson.fevers = this.amson.fevers.filter((fever) => fever.tick !== tick);
+    return true;
+  }
+
   private findNote(tick: number) {
     return this.amson.notes.find((note) => note.tick == tick);
+  }
+
+  private isValidDuration(from: number, to: number) {
+    const fromMs = this.amson.info.startAtMs + this.utility.durationByTick(from) * 1000;
+    const toMs = this.amson.info.startAtMs + this.utility.durationByTick(to) * 1000;
+
+    return this.amson.info.startAtMs <= fromMs && toMs <= this.amson.info.endAtMs;
+  }
+
+  private defaultLaneType() {
+    switch (this.amson.info.stage) {
+      case Amson.StageType.Live:
+        return Amson.LiveLaneType.kPositiveClockwise;
+      case Amson.StageType.Fashion:
+        return Amson.FashionLaneType.kCircle;
+      case Amson.StageType.Dance:
+        return Amson.DanceLaneType.kUpperLeft;
+    }
+  }
+
+  private checkRangeConflict(from: number, to: number) {
+    {
+      const conflictSection = this.amson.sections.find(
+        (section) => section.tick <= to && from <= section.tick + section.lengthTick
+      );
+      if (conflictSection != null) {
+        console.log('conflict with section', conflictSection);
+        return false;
+      }
+    }
+    {
+      const appealLength = this.utility.tickByDuration(Amson.Constants.AppealDuationSec);
+      const conflictAppeal = this.amson.appeals.find(
+        (appeal) => appeal.tick <= to && from <= appeal.tick + appealLength
+      );
+      if (conflictAppeal != null) {
+        console.log('conflict with appeal', conflictAppeal);
+        return false;
+      }
+    }
+    {
+      const feverLength = this.utility.tickByDuration(Amson.Constants.FeverDuraionSec);
+      const conflictFever = this.amson.fevers.find(
+        (fever) => fever.tick <= to && from <= fever.tick + feverLength
+      );
+      if (conflictFever != null) {
+        console.log('conflict with fever', conflictFever);
+        return false;
+      }
+    }
+
+    return true;
   }
 }
